bd09b6849804c5b8641cb001b618344a477b90e5
Adrita Samanta <adrita@Adritas-MacBook-Pro-2.local>
2024-07-23 14:28:36 -0400

    Tested Color Feature

--- program_code/color_feature.py
+++ program_code/color_feature.py
@@ -2,43 +2,49 @@ import cv2
 import numpy as np
 
 def quantize_h(H):
-    h = 0
-    if 0 <= H <= 20 or 316 <= H <= 359:
-        h = 0
-    elif 21 <= H <= 40:
-        h = 1
-    elif 41 <= H <= 75:
-        h = 2
-    elif 75 <= H <= 155:
-        h = 3
-    elif 156 <= H <= 190:
-        h = 4
-    elif 191 <= H <= 270:
-        h = 5
-    elif 271 <= H <= 295:
-        h = 6
-    elif 296 <= H <= 315:
-        h = 7
+    h = H
+    for i in range(H.shape[0]):
+        for j in range(H.shape[1]):
+            if 0 <= H[i][j] <= 20 or 316 <= H[i][j] <= 359:
+                h[i][j] = 0
+            elif 21 <= H[i][j] <= 40:
+                h[i][j] = 1
+            elif 41 <= H[i][j] <= 75:
+                h[i][j] = 2
+            elif 75 <= H[i][j] <= 155:
+                h[i][j] = 3
+            elif 156 <= H[i][j] <= 190:
+                h[i][j] = 4
+            elif 191 <= H[i][j] <= 270:
+                h[i][j] = 5
+            elif 271 <= H[i][j] <= 295:
+                h[i][j] = 6
+            elif 296 <= H[i][j] <= 315:
+                h[i][j] = 7
     return h
 
 def quantize_s(S):
-    s = 0
-    if 0 <= S < 0.2:
-        s = 0
-    elif 0.2 <= S < 0.7:
-        s = 1
-    elif 0.7 <= S < 1:
-        s = 2
+    s = S
+    for i in range(S.shape[0]):
+        for j in range(S.shape[1]):
+            if 0 <= S[i][j] < 0.2:
+                s[i][j] = 0
+            elif 0.2 <= S[i][j] < 0.7:
+                s[i][j] = 1
+            elif 0.7 <= S[i][j] < 1:
+                s[i][j] = 2
     return s
 
 def quantize_b(B):
-    b = 0
-    if 0 <= B < 0.2:
-        b = 0
-    elif 0.2 <= B < 0.7:
-        b = 1
-    elif 0.7 <= B < 1:
-        b = 2
+    b = B
+    for i in range(B.shape[0]):
+        for j in range(B.shape[1]):
+            if 0 <= B[i][j] < 0.2:
+                b[i][j] = 0
+            elif 0.2 <= B[i][j] < 0.7:
+                b[i][j] = 1
+            elif 0.7 <= B[i][j] < 1:
+                b[i][j] = 2
     return b
 
 def extract_color_features(FILENAME):
@@ -63,6 +69,6 @@ def extract_color_features(FILENAME):
     return normalized_feature_vector
 
 if __name__ == "__main__":
-    FILENAME = ""
+    FILENAME = "../tests/test_images/platypus.jpg"
     color_features = extract_color_features(FILENAME)
     print("Color Feature Vector (normalized):", color_features)
