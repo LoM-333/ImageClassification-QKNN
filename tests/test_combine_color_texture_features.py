3d61ba0ed9b0b7edc298593fcbc130c34ce598b5
Akshay Pespunuri <akshay.pespunuri@gmail.com>
2024-07-26 10:15:35 -0500

    fixed combining color and texture feature vectors

--- tests/test_combine_color_texture_features.py
+++ tests/test_combine_color_texture_features.py
@@ -6,12 +6,12 @@ from src.combine_color_texture_features import combine_color_texture_features
 
 class TestCombineColorTextureFeaturesExtraction(unittest.TestCase):
     def testCombineFeatures(self):
-        FILENAME = '../tests/test_images/platypus.jpg'
+        FILENAME = 'tests/test_images/platypus.jpg'
         combinedFeatures = combine_color_texture_features(FILENAME)
 
         self.assertIsInstance(combinedFeatures, np.ndarray)
 
-        expected_length = 512 + 256
+        expected_length = 80
         self.assertEqual(len(combinedFeatures), expected_length)
 
 if __name__ == '__main__':
